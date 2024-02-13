8adf6a065ff2716f8056fef1458fa8c84ce690b6
Maksim Isaev <maksim.isaev@combridge-itc.de>
2024-02-13 09:43:37 +0100

    Changed version to 0.0.9

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
+import { join } from 'path';
 import * as enums from './enums';
 import * as decorators from './decorators';
+
+export const PROTO_FOLDER_PATH = join(__dirname, '..', 'proto');
 
 export { enums, decorators };
